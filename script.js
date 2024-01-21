ede1428b954424e04b78468b94b27b77bc9b5da2
fanislala <137422059+fanislala@users.noreply.github.com>
2024-01-22 02:28:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function animateElement() {
+// script.js
+
+function animateSquare() {
     var element = document.querySelector('h1');
-    element.style.color = 'red';
+    element.style.color = 'Black';
     element.style.transition = 'color 1s';
+
+  var square = document.querySelector('.animated-square');
+
+    // Use anime.js for bouncing animation
+    anime({
+        targets: square,
+        translateY: [
+            { value: -100, duration: 500, easing: 'easeInOutQuad' },
+            { value: 0, duration: 500, easing: 'easeInOutQuad' }
+        ],
+        loop: true 
+    });
+}
